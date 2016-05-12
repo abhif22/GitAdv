f85db9459fbdbfaad80d6a534e7d3d4ce3f09730
Abhishek Srivastava <abhi.f18@gmail.com>
2016-05-12 10:09:11 +0530

    Installed MongoClient

--- Index.js
+++ Index.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var mongodb = require('mongodb').MongoClient;
 var app = express();
+
+mongodb.connect('mongodb//localhost:271017/dishes',function(err,db){
+	if(err)
+		console.log(err);
+});
 
 app.listen(3000,function(err){
 	if(err)
